aa963f9dc064895efab95434226e0929d6635ee9
Shanna Noe <snoe.prsvr@gmail.com>
2025-06-05 14:00:28 -0400

    feat: NavigationTab

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import ThemeToggle from "@/components/ThemeToggle";
 import Image from "next/image";
 import MoodHistoryEntry, { MoodEntry } from "@/components/MoodHistoryEntry";
-import NavigationTabs from "@/components/NavigationTabs"; // Ensure the correct path
+
 
 export default function Home() {
   const sampleEntry: MoodEntry = {
@@ -14,7 +14,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <ThemeToggle />
-      <NavigationTabs />
+    
        
 
         <MoodHistoryEntry entry={sampleEntry} />
