e87d586bb36dbc457483164819e475a07d590312
Shanna Noe <snoe.prsvr@gmail.com>
2025-06-06 09:24:46 -0400

    feat: read-me

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,22 @@
 "use client";
-import ThemeToggle from "@/components/ThemeToggle";
-import Image from "next/image";
-import Header from "@/components/Header";
-
-export default function Home() 
-
 import React from "react";
+
 import TrackerForm from "@/components/TrackerForm";
 
-
-export default function HomePage() {
-  return (
-    <div className="space-y-8">
-      <TrackerForm />
-    </div>
-
+export default function Home(): React.ReactElement {
   return (
     <>
-<div><Header /></div>
-  <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      
-      </main>
-    </div>
+      <div className="space-y-8">
+        <TrackerForm />
+      </div>
+      <div>
+     
+      </div>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+          {/* Add content here if needed */}
+        </main>
+      </div>
     </>
   );
 }
